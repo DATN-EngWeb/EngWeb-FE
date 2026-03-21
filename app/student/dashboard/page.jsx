a7e1dd784e77f87a0b0fac3288cb3421bd9e76a7
WhoamiSexy <22120074@student.hcmus.edu.vn>
2026-03-21 23:47:10 +0700

    fix: change server component to client component and fix skeleton in /student/test/attempt
    + change server component to client component
    + preload audio, html, image with Skeleton before go to UI for betteer UX

--- app/student/dashboard/page.jsx
+++ app/student/dashboard/page.jsx
@@ -1,5 +1,7 @@
+'use client';
+
 import StudentDashboard from '@/components/Dashboard/dashboard';
 
-export default async function Page() {
+export default function Page() {
   return <StudentDashboard />;
 }
