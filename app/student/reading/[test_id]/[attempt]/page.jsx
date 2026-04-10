e6fb9f42dcd1f41335ce7655b7221355f963994a
WhoamiSexy <22120074@student.hcmus.edu.vn>
2026-04-10 10:14:53 +0700

    feat: implement view detail test result after submit test listening (done)

--- app/student/reading/[test_id]/[attempt]/page.jsx
+++ app/student/reading/[test_id]/[attempt]/page.jsx
@@ -1,5 +1,25 @@
-import ReceptiveTestResult from '@/components/Student/ReceptiveTestResult/ReceptiveTestResult';
+'use client';
 
-export default function ReceptiveResultsPage() {
-  return <ReceptiveTestResult />;
+import ReceptiveTestResult from '@/components/Student/ReceptiveTestResult/ReceptiveTestResult';
+import React from 'react';
+import { useEffect, useState } from 'react';
+
+export default function ReceptiveResultsPage({ params }) {
+  const unwrappedParams = React.use(params);
+  const { test_id } = unwrappedParams;
+  const [history_id, setHistoryId] = useState(null);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const saved = window.sessionStorage.getItem('current_receptive_attempt');
+      if (saved) {
+        const savedData = JSON.parse(saved);
+        if (savedData.history_id) {
+          setHistoryId(savedData.history_id);
+        }
+      }
+    }
+  }, [test_id]);
+
+  return <ReceptiveTestResult historyId={history_id} />;
 }
