05e3afbd16f9877bd11855090e616641f6a8960e
WhoamiSexy <22120074@student.hcmus.edu.vn>
2026-06-17 01:16:34 +0700

    fix: delete unused route and delete w/s tab in dashboard

--- app/student/reading/[test_id]/results/[historyId]/page.jsx
+++
@@ -1,6 +0,0 @@
-import ReceptiveTestResult from '@/components/Student/ReceptiveTestResult/ReceptiveTestResult';
-
-export default async function ReceptiveResultsPage({ params }) {
-  const resolvedParams = await params;
-  return <ReceptiveTestResult mode="summary" params={resolvedParams} />;
-}
--- app/student/reading/[test_id]/results/[historyId]/review/page.jsx
+++
@@ -1,6 +0,0 @@
-import ReceptiveTestResult from '@/components/Student/ReceptiveTestResult/ReceptiveTestResult';
-
-export default async function ReceptiveReviewPage({ params }) {
-  const resolvedParams = await params;
-  return <ReceptiveTestResult mode="review" params={resolvedParams} />;
-}
